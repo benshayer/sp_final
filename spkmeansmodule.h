e1fcdf0e9d85c919d46713f7928533f5965912c1
bens-vayyar <ben.shayer@vayyar.com>
2021-08-13 18:18:31 +0300

    change name of get matrix eignVectors to sortedEignVectors
    be more informative

--- spkmeansmodule.h
+++ spkmeansmodule.h
@@ -3,6 +3,7 @@ double** ComputeNormalizedGraphLaplacian(double** wam, double** ddm_square, int 
 double** CreateWeightedAdjacencyMatrix(double** observations, int dim, int n);
 void getMatrixSortedEignVectors(double** matrixA, double** matrixV, double** matrixU, int n, int k);
 int* initDataPoints(char* filename, double ***data_vectors);
+void getMatrixSortedEignVectors(double** matrixA, double** matrixV, double** matrixU, int n, int k);
 void JacobiAlgorithm(double** matrixA,double** matrixV, int n);
 void LaplacianNorm_helper(double** Lnorm, int n);
 void MatrixMultiply_helper(double** matrixA, double** matrixB, double** result, int n);
