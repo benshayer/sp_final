b7ced6c0e6b1f28be3ec330fe848131f8f2b5ae8
bens-vayyar <ben.shayer@vayyar.com>
2021-08-14 18:09:31 +0300

    add flowSPK

--- spkmeansmodule.h
+++ spkmeansmodule.h
@@ -1,7 +1,9 @@
 double calcWeight(double* observ1, double* observ2, int dim);
 double** ComputeNormalizedGraphLaplacian(double** wam, double** ddm_square, int n);
 double** CreateWeightedAdjacencyMatrix(double** observations, int dim, int n);
+void flowSPKforC(double** observations, int n, int dim, int k,int max_iter);
 void getMatrixSortedEignVectors(double** matrixA, double** matrixV, double** matrixU, int n, int k);
+double** getNewDataPointsDimK(double** observations, int n, int dim, int* k);
 int* initDataPoints(char* filename, double ***data_vectors);
 void getMatrixSortedEignVectors(double** matrixA, double** matrixV, double** matrixU, int n, int k);
 void JacobiAlgorithm(double** matrixA,double** matrixV, int n);
