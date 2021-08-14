602535829dc03ffab34e0ff0bb6d344f35417c79
bens-vayyar <ben.shayer@vayyar.com>
2021-08-14 16:42:24 +0300

    keep h file sorted by names

--- spkmeansmodule.h
+++ spkmeansmodule.h
@@ -1,6 +1,7 @@
 double calcWeight(double* observ1, double* observ2, int dim);
 double** ComputeNormalizedGraphLaplacian(double** wam, double** ddm_square, int n);
 double** CreateWeightedAdjacencyMatrix(double** observations, int dim, int n);
+double** DiagonalDegreeMatrix(double** matrix, int n);
 void flowSPKforC(double** observations, int n, int dim, int k,int max_iter);
 void getMatrixSortedEignVectors(double** matrixA, double** matrixV, double** matrixU, int n, int k);
 double** getNewDataPointsDimK(double** observations, int n, int dim, int* k);
@@ -10,5 +11,5 @@ void JacobiAlgorithm(double** matrixA,double** matrixV, int n);
 void LaplacianNorm_helper(double** Lnorm, int n);
 void MatrixMultiply_helper(double** matrixA, double** matrixB, double** result, int n);
 int TheEigengapHeuristic(double* eignvalues, int len);
-double** DiagonalDegreeMatrix(double** matrix, int n);
+
 void printMatrix(double** matrix, int a, int b);
