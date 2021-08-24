3a3e45165e436862bb67f8de2bcaa2c6f52c98e5
bens-vayyar <ben.shayer@vayyar.com>
2021-08-24 15:55:15 +0300

    update setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, Extension
+
+
+setup(name='spkmeans',
+      version='1.0',
+      description='My implemantation to calculate kmeans from given Data Points and Centroids. By Ben and Gilad',
+      ext_modules=[Extension('spkmeans', sources=['spkmeansmodule.c'])])
